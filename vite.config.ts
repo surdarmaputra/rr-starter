c856568a16b4c9cb0e288cc8f79ea076bf50d24d
Surya Darma Putra <surya.igede@gmail.com>
2024-12-31 21:50:50 +0800

    chore: add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 import { reactRouter } from '@react-router/dev/vite';
 import autoprefixer from 'autoprefixer';
 import tailwindcss from 'tailwindcss';
@@ -5,11 +6,17 @@ import tailwindcssNesting from 'tailwindcss/nesting';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const vitePlugins = [reactRouter(), tsconfigPaths()];
+
+if (process.env.NETLIFY_DEPLOYMENT === 'true') {
+  vitePlugins.push(netlifyPlugin());
+}
+
 export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcssNesting, tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: vitePlugins,
 });
