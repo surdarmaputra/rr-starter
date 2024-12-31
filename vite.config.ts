060836c685273d1494748866485d1cc129d262e7
Surya Darma Putra <surya.igede@gmail.com>
2024-12-31 21:17:10 +0800

    chore: add prettier and tailwindcss nesting:

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import tailwindcssNesting from 'tailwindcss/nesting';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [tailwindcssNesting, tailwindcss, autoprefixer],
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
