669a24f6c70fe80a40313fd918f406b20820e1fd
surdarmaputra <surya.igede@gmail.com>
2025-02-25 09:37:03 +0700

    wip: adjust sonner style

--- app/modules/core/components/Sonner/Sonner.tsx
+++ app/modules/core/components/Sonner/Sonner.tsx
@@ -7,14 +7,22 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       className="toaster group"
       toastOptions={{
+        unstyled: true,
         classNames: {
-          toast: `group toast group-[.toaster]:bg-white group-[.toaster]:text-letter-title
-            group-[.toaster]:border-divider group-[.toaster]:shadow-lg`,
+          toast: `
+              bg-white dark:bg-accent
+              text-letter-title border-divider shadow-lg
+              px-4 py-2.5 text-sm border rounded-lg
+              flex items-center gap-2.5 w-[240px]
+            `,
           description: 'group-[.toast]:text-letter-body',
           actionButton:
             'group-[.toast]:bg-backdrop group-[.toast]:text-letter-inverted',
           cancelButton:
             'group-[.toast]:bg-accent group-[.toast]:text-letter-body',
+          success: 'group-[.toast]:bg-warning',
+          content: 'flex flex-col gap-2',
+          closeButton: '!bg-white dark:!bg-accent border-divider',
         },
       }}
       {...props}
